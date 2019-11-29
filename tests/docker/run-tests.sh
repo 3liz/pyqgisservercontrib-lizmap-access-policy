a0bf68aaf8ad497f8357682bace36b72091a52e5
David Marteau <dmarteau@3liz.com>
2019-11-29 13:22:42 +0100

    Change docker-* startup scripts

--- tests/docker/run-tests.sh
+++ tests/docker/run-tests.sh
@@ -2,19 +2,28 @@
 
 set -e
 
-# Add /.local to path
-export PATH=$PATH:/.local/bin
+# Qgis need a HOME
+export HOME=/home/qgis
 
-echo "-- HOME is $HOME"
+if [ "$(id -u)" = '0' ]; then
 
 echo "Installing python packages..."
-pip3 install -U --user -q setuptools
-pip3 install --no-warn-script-location -q --user --prefer-binary -r requirements.tests
-pip3 install --no-warn-script-location -q --user --prefer-binary -r requirements.txt 
+pip3 install -U -q setuptools
+pip3 install --no-warn-script-location -q --prefer-binary -r requirements.tests
+pip3 install --no-warn-script-location -q --prefer-binary -r requirements.txt 
 
-pip3 install -q --user -e ./ 
+pip3 install -e ./ 
 
 echo "Installed contributions: $(pip list -l | grep pyqgiservercontrib)"
+
+mkdir -p $HOME
+chown -R $BECOME_USER:$BECOME_USER $HOME
+
+exec gosu $BECOME_USER:$BECOME_USER "$BASH_SOURCE" $@
+
+fi
+
+echo "-- HOME is $HOME"
 
 export QGIS_DISABLE_MESSAGE_HOOKS=1
 export QGIS_NO_OVERRIDE_IMPORT=1
