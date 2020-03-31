0bd31a7538f48ec32b412e7dce84d96463cd8686
David Marteau <dmarteau@3liz.com>
2020-03-31 17:10:07 +0200

    Add tests

--- tests/unittests/test_policy.py
+++ tests/unittests/test_policy.py
@@ -42,5 +42,30 @@ class Tests(HTTPTestCase):
         # Check that there is only one exposed pyqgiswps_test
         idents = [x for x in exposed.split() if x.startswith('pyqgiswps_test:')]
         assert idents == ['pyqgiswps_test:testsimplevalue']
-       
+
+
+    def test_getcapabilities_3(self):
+        """ Test access policy
+        """
+        uri = ('?service=WPS&request=GetCapabilities&MAP=france_parts')
+        rv = self.client.get(uri, headers={ 'X-Lizmap-User': 'john' })
+        assert rv.status_code == 200
+
+        exposed = rv.xpath_text('/wps:Capabilities'
+                                  '/wps:ProcessOfferings'
+                                  '/wps:Process'
+                                  '/ows:Identifier')
+        # Check that there is only one exposed pyqgiswps_test
+        idents = [x for x in exposed.split() if x.startswith('pyqgiswps_test:')]
+        assert idents == ['pyqgiswps_test:testcopylayer']
+ 
+
+    def test_execute_return_ok(self):
+        """ Test map profile 
+        """
+        uri = ('?service=WPS&request=Execute&Identifier=pyqgiswps_test:testcopylayer&Version=1.0.0&MAP=france_parts'
+                   '&DATAINPUTS=INPUT=france_parts%3BOUTPUT=france_parts_2')
+        rv = self.client.get(uri, headers={ 'X-Lizmap-User': 'john' })
+        assert rv.status_code == 200
+
 
